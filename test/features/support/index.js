dcd93d9945d302ad2198e66988f43643c1e0f619
maria er <errfig.m@gmail.com>
2023-06-09 13:03:49 +0100

    index.js

--- test/features/support/index.js
+++ test/features/support/index.js
@@ -2,8 +2,8 @@ var reporter = require('cucumber-html-reporter');
 
 var options = {
         theme: 'bootstrap',
-        jsonFile: './cucumber_report.json',
-        output: './cucumber_report.html',
+        jsonFile: './reports/cucumber_report.json',
+        output: './reports/cucumber_report.html',
         reportSuiteAsScenarios: true,
         scenarioTimestamp: true,
         launchReport: true,
